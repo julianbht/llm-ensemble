96083a87666d5a619a25e0bb0660f4612bef7356
Julian Hergoss <julian.hergoss@web.de>
2026-01-01 10:48:48 -0500

    .

--- src/llm_ensemble/evaluate/schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Schemas for the evaluate CLI."""
-
-from llm_ensemble.evaluate.schemas.evaluation_metrics import (
-    EvaluationMetrics,
-    ClassMetrics,
-)
-
-__all__ = ["EvaluationMetrics", "ClassMetrics"]

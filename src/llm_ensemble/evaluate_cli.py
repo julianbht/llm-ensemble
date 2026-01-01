d0e56dce220a3dae4725771c578c532f46d77cdd
Julian Hergoss <julian.hergoss@web.de>
2026-01-01 10:49:24 -0500

    .

--- src/llm_ensemble/evaluate_cli.py
+++ src/llm_ensemble/evaluate_cli.py
@@ -1,4 +1,4 @@
-"""Evaluate CLI - Compute metrics and generate HTML reports.
+"""Evaluate CLI - Compute metrics and generate reports.
 """
 
 from llm_ensemble.libs.runtime.env import load_runtime_config

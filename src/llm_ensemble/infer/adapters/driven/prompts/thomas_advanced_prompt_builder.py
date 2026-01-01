2f11cc069fd3be3ad41ccbe8da24673fe2734dd3
Julian Hergoss <julian.hergoss@web.de>
2026-01-01 05:00:09 -0500

    thomas advanced prompt to be optimal according to mic paper

--- src/llm_ensemble/infer/adapters/driven/prompts/thomas_advanced_prompt_builder.py
+++ src/llm_ensemble/infer/adapters/driven/prompts/thomas_advanced_prompt_builder.py
@@ -58,8 +58,8 @@ class ThomasAdvancedPromptBuilder(ForBuildingPrompts):
         Measure how trustworthy the web page is (T).
         Consider the aspects above and the relative importance of each, and decide on a final score (O).
 
-        Produce a JSON array of scores without providing any reasoning.
-        Example: {{"M": 2, "T": 1, "O": 1}}""")
+        Produce a JSON object scores without providing any reasoning.
+        Example: {"M": 2, "T": 1, "O": 1}""")
 
     def __init__(self):
         """Initialize builder and create cached PromptBuilder entity."""

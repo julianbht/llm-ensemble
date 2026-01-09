9ebebad9ad41f75f50f5263af1ea49ccf9a71e39
Julian Hergoss <julian.hergoss@web.de>
2026-01-09 00:31:41 -0800

    update trec prompt

--- src/llm_ensemble/infer/adapters/driven/prompts/thomas_advanced_trec_prompt_builder.py
+++ src/llm_ensemble/infer/adapters/driven/prompts/thomas_advanced_trec_prompt_builder.py
@@ -26,8 +26,8 @@ class ThomasAdvancedTrecPromptBuilder(ForBuildingPrompts):
     - {document} - The document text
     """
 
-    TEMPLATE_NAME = "thomas-advanced-trec"
-    TEMPLATE_ID = uuid.uuid5(uuid.NAMESPACE_DNS, "llm-ensemble.thomas-advanced-trec-v1")
+    TEMPLATE_NAME = "thomas-advanced-trec-v2"
+    TEMPLATE_ID = uuid.uuid5(uuid.NAMESPACE_DNS, "llm-ensemble.thomas-advanced-trec-v2")
     TEMPLATE_TEXT = dedent("""\
         Given a query and a web page, you must provide a score on an integer scale of 0 to 3 with the following meanings:
 
